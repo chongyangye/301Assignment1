c0e92f0f60db327381f34325637da15b62b55a2e
chongyangye <cye2@ualberta.ca>
2014-01-31 06:02:59 -0700

    Finally

--- src/com/example/assignment1/MainActivity.java
+++ src/com/example/assignment1/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.assignment1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +13,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -35,9 +42,32 @@ public class MainActivity extends Activity {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			Intent inte =new Intent();
-			inte.setClass(MainActivity.this, PreBut.class);
-			MainActivity.this.startActivity(inte);
+			
+			String string= "";
+			try{
+				FileInputStream fis = openFileInput("file6.sav");
+				BufferedReader er = new BufferedReader(new InputStreamReader(fis));
+				string= er.readLine();
+				if(string==null){
+					String masEmp = "All counter has been removed, please create a new one";
+					Toast.makeText(MainActivity.this, 
+							masEmp, Toast.LENGTH_LONG).show();
+				}else{
+					Intent newc = new Intent();
+					newc.setClass(MainActivity.this,PreBut .class);
+					MainActivity.this.startActivity(newc);
+				}
+			}catch (FileNotFoundException e) {
+			// TODO: handle exception
+			String masErr = "There is No counter in the File";
+			Toast.makeText(MainActivity.this, 
+					masErr, Toast.LENGTH_LONG).show();
+			e.printStackTrace();
+			}catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			}
+			
 		}
 	}
 	class MyButtonListener implements OnClickListener{
